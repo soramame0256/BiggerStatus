2fed592d911934b848486acd4a1dc306e8ebd007
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-05-15 18:11:17 +0900

    fixed an issue that not work if mob had crowd control

--- src/main/java/com/github/soramame0256/biggerstatus/BiggerStatus.java
+++ src/main/java/com/github/soramame0256/biggerstatus/BiggerStatus.java
@@ -36,7 +36,7 @@ public class BiggerStatus {
     public static final String MOD_ID = "biggerstatus";
     public static final String MOD_NAME = "BiggerStatus";
     public static final String VERSION = "1.0-SNAPSHOT";
-    public static final Pattern HEALTH_NAME_TAG_REG = Pattern.compile("§4\\[§.?\\|§?.?\\|§?.?\\|§?.?\\|§?.?\\|.*§?.?\\|§?.?\\|§?.?\\|§?.?\\|§?.?\\|§4] (?<status>.*)");
+    public static final Pattern HEALTH_NAME_TAG_REG = Pattern.compile(".*§4\\[§.?\\|§?.?\\|§?.?\\|§?.?\\|§?.?\\|.*§?.?\\|§?.?\\|§?.?\\|§?.?\\|§?.?\\|§4] (?<status>.*)");
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
