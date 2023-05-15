91dd6e11419e10d729263f7f49bd3bb9eaa5b108
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-05-15 18:12:23 +0900

    Edit ver

--- src/main/java/com/github/soramame0256/biggerstatus/BiggerStatus.java
+++ src/main/java/com/github/soramame0256/biggerstatus/BiggerStatus.java
@@ -35,7 +35,7 @@ public class BiggerStatus {
 
     public static final String MOD_ID = "biggerstatus";
     public static final String MOD_NAME = "BiggerStatus";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.0-SNAPSHOT-fix.1";
     public static final Pattern HEALTH_NAME_TAG_REG = Pattern.compile(".*§4\\[§.?\\|§?.?\\|§?.?\\|§?.?\\|§?.?\\|.*§?.?\\|§?.?\\|§?.?\\|§?.?\\|§?.?\\|§4] (?<status>.*)");
 
     /**
